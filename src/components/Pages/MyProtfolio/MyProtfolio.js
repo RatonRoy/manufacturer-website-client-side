1afc0c5cdc15240007a8ed64ac35c4c17bd15053
RatonRoy <royraton18@gmail.com>
2022-05-30 20:29:44 +0600

    change text

--- src/components/Pages/MyProtfolio/MyProtfolio.js
+++ src/components/Pages/MyProtfolio/MyProtfolio.js
@@ -61,53 +61,57 @@ const MyProtfolio = () => {
 				</div>
 				<div>
 					<h1 className='text-center text-2xl py-6'>My Projects </h1>
-					<div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-3 gap-4 py-8">
+					<div className="grid grid-cols-1 md:grid-cols-2 md:grid-cols-3 gap-4 py-8 px-10">
 						<div className=" py-8">
 							<h1 className='text-2xl'> Group portfolio </h1>
-							<a href="https://ratonroy.github.io/Group-protfolio/"> 
-							https://ratonroy.github.io/Group-protfolio/
+							<a href="https://ratonroy.github.io/Group-protfolio/">
+								https://ratonroy.github.io/Group-protfolio/
 							</a>
 						</div>
 						<div className="py-8">
 							<h1 className='text-2xl'> Fruit Store </h1>
 							<a href="https://glistening-buttercream-7e6192.netlify.app/">
-							https://glistening-buttercream-7e6192.netlify.app/
+								https://glistening-buttercream-7e6192.netlify.app/
 							</a>
 						</div>
 						<div className="py-8">
 							<h1 className='text-2xl'> Tutor-of-science </h1>
 							<a href="https://tutor-of-science.web.app/">
-							https://tutor-of-science.web.app/
+								https://tutor-of-science.web.app/
 							</a>
 						</div>
 
 					</div>
 				</div>
 				<div className="">
-					<h1 className="text-2xl"> I know those technologies </h1>
-					<ul className='list-disc pl-4 flex'>
-						<li>HTML5 </li>
-						<li>Raw CSS </li>
-						<li> Javascript with ES6 </li>
-						<li>Bootstrap </li>
-						<li> React Bootstrap </li>
-						<li>Figma to html css </li>
-						<li> PSD  to html css </li>
-						<li> PNG   to html css </li>
-						<li>Responsive design </li>
-						<li>Tailwindcss</li>
-						<li>daisyUI Tailwindcss components</li>
-						<li> React </li>
-						<li> Firebase </li>
-						<li> React firebase hooks  </li>
-						<li> React Router </li>
-						<li> React form </li>
-						<li>JSON </li>
-						<li> API </li>
-						<li> Node   </li>
-						<li> MongoDb   </li>
-						<li> Express js   </li>
-						<li> git and github   </li>
+					<h1 className="text-2xl my-8 text-center"> I know those technologies </h1>
+					<ul className='list-disc pl-4 grid grid-cols-1 md:grid-cols-2 md:px-8 w-1/2 mx-auto'>
+						<div>
+							<li>HTML5 </li>
+							<li>Raw CSS </li>
+							<li> Javascript with ES6 </li>
+							<li>Bootstrap </li>
+							<li> React Bootstrap </li>
+							<li>Figma to html css </li>
+							<li> PSD  to html css </li>
+							<li> PNG   to html css </li>
+							<li>Responsive design </li>
+						</div>
+						<div>
+							<li>Tailwindcss</li>
+							<li>daisyUI Tailwindcss components</li>
+							<li> React </li>
+							<li> Firebase </li>
+							<li> React firebase hooks  </li>
+							<li> React Router </li>
+							<li> React form </li>
+							<li>JSON </li>
+							<li> API </li>
+							<li> Node   </li>
+							<li> MongoDb   </li>
+							<li> Express js   </li>
+							<li> git and github   </li>
+						</div>
 					</ul>
 				</div>
 			</div>
